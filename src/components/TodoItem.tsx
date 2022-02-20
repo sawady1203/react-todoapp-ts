34f6f796e451632466e8d8d6429efb4e710b0f02
sawady1203 <sawady.dev@gmail.com>
2022-02-20 12:55:00 +0900

    １アイテムだけリスト表示させるようにした

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Task } from "./Types";
 
-function TodoItem() {
-  return <div>TodoItem</div>;
+function TodoItem({ id, title }: Task) {
+  return (
+    <div>
+      <h3>
+        {id} : {title}
+      </h3>
+    </div>
+  );
 }
 
 export default TodoItem;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import { Task } from "./Types";
 
 function TodoList() {
+  const tasks: Task[] = [
+    {
+      id: 0,
+      title: "test",
+      isActive: false,
+    },
+  ];
   return (
     <div>
       <h2>TodoList</h2>
-      <TodoItem />
+      <TodoItem {...tasks[0]} />
     </div>
   );
 }
